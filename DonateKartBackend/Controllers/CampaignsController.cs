8025bda0c49caed461d9cb9bc92aeb1448209f73
Abhinav9550 <63183306+Abhinav9550@users.noreply.github.com>
2021-06-27 10:41:55 +0530

    Confirming the commit. All services are tested and functioning as expected

---
+++ DonateKartBackend/Controllers/CampaignsController.cs
@@ -0,0 +1,84 @@
+﻿using DonateKartBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DonateKartBackend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CampaignsController
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> getCampaigns()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, "https://testapi.donatekart.com/api/campaign");
+                    var response1 = await client.SendAsync(request);
+                    if (response1.IsSuccessStatusCode)
+                    {
+                        var responseStream = await response1.Content.ReadAsStreamAsync();
+                        StreamReader reader = new StreamReader(responseStream);
+                        string text = reader.ReadToEnd();
+                        var resp = JsonConvert.DeserializeObject<List<Campaigns_Api_Response>>(text);
+                        // var resp = JsonConvert.DeserializeObject<List<Tuple<(string title, DateTime endDate, double totalAmount, double backersCount)>>> (text);
+                        return new OkObjectResult(resp.OrderByDescending(c => c.totalAmount));
+                    }
+                    else
+                        return new BadRequestObjectResult("Call to api failed");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+
+        [HttpGet]
+        [Route("{type}")]
+        public async Task<IActionResult> getCampaignsByInput(string type)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, "https://testapi.donatekart.com/api/campaign");
+                    var response1 = await client.SendAsync(request);
+                    if (response1.IsSuccessStatusCode)
+                    {
+                        var responseStream = await response1.Content.ReadAsStreamAsync();
+                        StreamReader reader = new StreamReader(responseStream);
+                        string text = reader.ReadToEnd();
+                        var resp = JsonConvert.DeserializeObject<List<Campaigns_Api_Response2>>(text);
+                        if (type == "active")
+                            resp = resp.Where(c => c.endDate >= DateTime.UtcNow && c.created != null && ((DateTime.UtcNow  - c.created )?.TotalDays ?? 0) <= 30).ToList();
+                        else if (type == "closed")
+                            resp = resp.Where(c => c.endDate < DateTime.UtcNow && c.procuredAmount >= c.totalAmount).ToList();
+                        else
+                            return new OkObjectResult("please enter either 'active' or 'closed' instead of => " + type);
+                        return new OkObjectResult(resp);
+
+                    }
+                    else
+                        return new BadRequestObjectResult("Call to api failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ DonateKartBackend/Models/Campaigns_Api_Response.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonateKartBackend.Models
+{
+
+
+
+    public class Campaigns_Api_Response
+    {
+        public string title { get; set; }
+        public double totalAmount { get; set; }
+        public double backersCount { get; set; }
+        public DateTime endDate { get; set; }
+
+    }
+    public class Campaigns_Api_Response2
+    {
+        public string code { get; set; }
+        public string title { get; set; }
+        public bool featured { get; set; }
+        public double priority { get; set; }
+        public string shortDesc { get; set; }
+        public string imageSrc { get; set; }
+        public DateTime? created { get; set; }
+        public DateTime endDate { get; set; }
+        public double totalAmount { get; set; }
+        public double procuredAmount { get; set; }
+        public double totalProcured { get; set; }
+        public double backersCount { get; set; }
+        public int? categoryId { get; set; }
+        public string ngoCode { get; set; }
+        public string ngoName { get; set; }
+        public int daysLeft { get; set; }
+        public double percentage { get; set; }
+    }
+}
